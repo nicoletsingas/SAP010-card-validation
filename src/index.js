5f829f7672bd77cc3b724f79805c1de7e7f71b6c
Nicole Tsingas <nicolecass27@gmail.com>
2023-04-21 17:14:20 -0300

    Criado DIV back-card no HTML. Finalizado o estilo das paginas index.html e validation.html; Feito animação de girar o cartão com CSS e JS; Feito primeiras funcionalidades do DOM: dados dos inputsXcard.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import validator from './validator.js';
 console.log(validator);
 
+/* Mostrar os dados dos inputs no cartão*/
+
 const cardNumberInput = document.querySelector('#card-number-input'); //salvando o elemento do input dentro da variavel
 const cardNumber = document.querySelector('.card-number');
 
@@ -13,18 +15,37 @@ const expMounthCard = document.querySelector('#exp-mm');
 const expYearInput = document.querySelector('#years');
 const expYearCard = document.querySelector('#exp-yy');
 
+const cvvInput = document.querySelector('#cvv-card-input');
+const cvvCard = document.querySelector('.cvv-field');
+
 cardNumberInput.oninput = function (event){
-    cardNumber.innerText = event.target.value; //mostra o valor do input no campo do cartão
-}
+    cardNumber.innerText = event.target.value //mostra o valor do input no campo do cartão
+};
 
 cardNameInput.oninput = function (event){
-    cardName.innerText = event.target.value;
-}
+    cardName.innerText = event.target.value
+};
 
 expMounthInput.oninput = function (event){
-    expMounthCard.innerText = event.target.value;
-}
+    expMounthCard.innerText = event.target.value
+};
 
-expYearInput.oninput = function (e){
-    expYearCard.innerText = event.target.value;
-}
+expYearInput.oninput = function (event){
+    expYearCard.innerText = event.target.value
+};
+
+cvvInput.oninput = function (e){
+    cvvCard.innerText = e.target.value /* "e" de event*/
+};
+
+/* Animação rotação do cartão conforme passa o mouse em cima do input CVV*/
+cvvInput.onmouseenter = function (){
+	document.querySelector('.front-card').style.transform = 'perspective(1000px) rotateY(180deg)'
+	document.querySelector('.back-card').style.transform = 'perspective(1000px) rotateY(0deg)'
+};
+
+// onmouseleave - quando o mouse "sair" do campo
+cvvInput.onmouseleave = function (){ 
+	document.querySelector('.front-card').style.transform = 'perspective(1000px) rotateY(0deg)'
+	document.querySelector('.back-card').style.transform = 'perspective(1000px) rotateY(180deg)'
+};
