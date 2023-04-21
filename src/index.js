e7093e76f31e58c12c864693925a3a87362123dc
Nicole Tsingas <nicolecass27@gmail.com>
2023-04-21 14:14:47 -0300

    Feito os primeiros passos em JS DOM na index.js; Ao digitar os valores nos inputs, estes são mostrados no cartão, feito funcionalidade atraves do DOM - querySelector;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
-
 console.log(validator);
+
+const cardNumberInput = document.querySelector('#card-number-input'); //salvando o elemento do input dentro da variavel
+const cardNumber = document.querySelector('.card-number');
+
+const cardNameInput = document.querySelector('#card-name-input');
+const cardName = document.querySelector('#full-name-card');
+
+const expMounthInput = document.querySelector('#mounths');
+const expMounthCard = document.querySelector('#exp-mm');
+
+const expYearInput = document.querySelector('#years');
+const expYearCard = document.querySelector('#exp-yy');
+
+cardNumberInput.oninput = function (event){
+    cardNumber.innerText = event.target.value; //mostra o valor do input no campo do cartão
+}
+
+cardNameInput.oninput = function (event){
+    cardName.innerText = event.target.value;
+}
+
+expMounthInput.oninput = function (event){
+    expMounthCard.innerText = event.target.value;
+}
+
+expYearInput.oninput = function (e){
+    expYearCard.innerText = event.target.value;
+}
