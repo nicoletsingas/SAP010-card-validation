47fcfca8d49ad487cb1c41e210ccc36f5f0279e3
Nicole Tsingas <nicolecass27@gmail.com>
2023-08-31 17:50:50 -0300

    removendo comentários

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import validator from './validator.js';
 
-/* Mostrar os dados dos inputs no cartão*/
-
-const cardNumberInput = document.querySelector('#card-number-input'); //salvando o elemento do input dentro da variavel
+const cardNumberInput = document.querySelector('#card-number-input'); 
 const cardNumber = document.querySelector('.card-number');
 
 const cardNameInput = document.querySelector('#card-name-input');
@@ -19,8 +17,8 @@ const cvvCard = document.querySelector('.cvv-field');
 
 const formSubmit = document.querySelector('#form');
 
-cardNumberInput.oninput = function (event){ //oninput puxa o valor do input
-  cardNumber.innerText = event.target.value //mostra o valor do input no campo do cartão
+cardNumberInput.oninput = function (event){
+  cardNumber.innerText = event.target.value
 };
 
 cardNameInput.oninput = function (event){
@@ -36,29 +34,27 @@ expYearInput.oninput = function (event){
 };
 
 cvvInput.oninput = function (e){
-  cvvCard.innerText = e.target.value /* "e" de event*/
+  cvvCard.innerText = e.target.value
 };
 
 formSubmit.onsubmit = function (e){
-  e.preventDefault(); //não envia o formulario do evento submit
-  const cardNumberToValidate = cardNumberInput.value //pegando valor do input e armazenando em uma variavel
-  const validNumber = validator.isValid(cardNumberToValidate) // chamar a função que esta na pagina validator.js e enviar o parametro
+  e.preventDefault(); 
+  const cardNumberToValidate = cardNumberInput.value 
+  const validNumber = validator.isValid(cardNumberToValidate) 
   if (validNumber === true){
     alert("Seu cartão de Crédito é Válido! Aproveite suas compras!!")
-  }else{
+  } else {
     alert("Seu cartão de Crédito não é Válido! Verifique o número digitado ou entre em contato com seu banco!") 
   }
   const finalMaskedNumber = validator.maskify(cardNumberToValidate)
   cardNumber.innerText = finalMaskedNumber
 };
 
-/* Animação rotação do cartão conforme passa o mouse em cima do input CVV*/
 cvvInput.onmouseenter = function (){
   document.querySelector('.front-card').style.transform = 'rotateY(180deg)'
   document.querySelector('.back-card').style.transform = 'rotateY(0deg)'
 };
 
-// onmouseleave - quando o mouse "sair" do campo
 cvvInput.onmouseleave = function (){ 
   document.querySelector('.front-card').style.transform = 'rotateY(0deg)'
   document.querySelector('.back-card').style.transform = 'rotateY(180deg)'
